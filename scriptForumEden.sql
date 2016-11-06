677e3f39d7f732dda4bf29372fb8914c80e47eb2
Picsou <soupic@protonmail.com>
2016-11-06 17:53:04 +0100

    Modification

--- scriptForumEden.sql
+++ scriptForumEden.sql
@@ -39,7 +39,7 @@ CREATE TABLE user(
         age      Int ,
         pays     Varchar (45) ,
         sex      Bool ,
-        id_group Int ,
+        id_droits Int ,
         PRIMARY KEY (id )
 )ENGINE=InnoDB;
 
@@ -48,7 +48,7 @@ CREATE TABLE user(
 # Table: group
 #------------------------------------------------------------
 
-CREATE TABLE group(
+CREATE TABLE droits(
         id     int (11) Auto_increment  NOT NULL ,
         status Varchar (45) ,
         PRIMARY KEY (id )
@@ -69,6 +69,6 @@ CREATE TABLE topics(
 
 ALTER TABLE post ADD CONSTRAINT FK_post_id_user FOREIGN KEY (id_user) REFERENCES user(id);
 ALTER TABLE post ADD CONSTRAINT FK_post_id_topics FOREIGN KEY (id_topics) REFERENCES topics(id);
-ALTER TABLE user ADD CONSTRAINT FK_user_id_group FOREIGN KEY (id_group) REFERENCES group(id);
+ALTER TABLE user ADD CONSTRAINT FK_user_id_droits FOREIGN KEY (id_droits) REFERENCES droits(id);
 ALTER TABLE topics ADD CONSTRAINT FK_topics_id_user FOREIGN KEY (id_user) REFERENCES user(id);
 ALTER TABLE topics ADD CONSTRAINT FK_topics_id_section FOREIGN KEY (id_section) REFERENCES section(id);
